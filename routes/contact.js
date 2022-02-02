3ed267acb399bc8a9de4b2a13ab01a526f211a63
Juan Manuel Munoz <jm.munoz14@uniandes.edu.co>
2022-02-02 00:29:29 -0500

    finished most of the web

---
+++ routes/contact.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('contact', { title: 'Contact me' });
+});
+
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'My portfolio' });
 });
 
-router.get('/aboutme', function(req, res, next) {
+router.get('/aboutme', function (req, res, next) {
   res.render('aboutMe', { title: 'About Me' });
+});
+
+router.get('/downloadcv', function (req, res, next) {
+  res.download('public/downloads/CV-JuanMMuñozR.pdf');
+});
+
+router.get('/projects', function (req, res, next) {
+  res.render('projects', { title: 'My Projects' });
+});
+
+router.get('/services', function (req, res, next) {
+  res.render('services', { title: 'My Projects' });
 });
 
 module.exports = router;
