7f042fdb1019bc1f8a9b26186b0ec03770e91b07
Juan Manuel Munoz <jm.munoz14@uniandes.edu.co>
2022-02-16 10:56:14 -0500

    revoked password

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
-
-let URI = "mongodb+srv://dbadmin:DAC1HbNR31v39G6F@cluster0.kdkux.mongodb.net/mydb?retryWrites=true&w=majority";
+let password = "<InsertPasswordHere>"
+let URI = "mongodb+srv://dbadmin:" + password + "@cluster0.kdkux.mongodb.net/mydb?retryWrites=true&w=majority";
 let mongoose = require('mongoose');
 
 module.exports = function () {
